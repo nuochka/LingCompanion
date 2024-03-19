c9fe0550c3ff6c86e3f981ec98c653e88b8ada3b
nuochka <140435213+nuochka@users.noreply.github.com>
2024-03-19 18:00:43 +0100

    Added logout button and functional on home page

--- app/src/main/java/com/app/lingcompanion/HomeActivity.kt
+++ app/src/main/java/com/app/lingcompanion/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.app.lingcompanion
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,14 +13,30 @@ import com.google.firebase.auth.FirebaseAuth
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
     private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        //Logout button
+        binding.btnLogout.setOnClickListener {
+            if (auth.currentUser != null) {
+                auth.signOut()
+                Intent(this, LoginActivity::class.java).also { intent ->
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                }
+                Toast.makeText(this, "Logout completed successfully", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "No user is currently signed in", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/com/app/lingcompanion/LoginActivity.kt
+++ app/src/main/java/com/app/lingcompanion/LoginActivity.kt
@@ -13,100 +13,97 @@ import com.app.lingcompanion.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.jakewharton.rxbinding2.widget.RxTextView
 import io.reactivex.Observable
-import kotlin.math.log
 
 @SuppressLint("CheckResult")
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //Auth
+        // Initialize Firebase Auth
         auth = FirebaseAuth.getInstance()
 
-        binding.btnLogin.setOnClickListener{
-            startActivity(Intent(this, HomeActivity::class.java))
-        }
-        //Username validation
+        // Username validation
         val usernameStream = RxTextView.textChanges(binding.etEmail)
             .skipInitialValue()
             .map { username ->
                 username.isEmpty()
             }
+
         usernameStream.subscribe {
-            showTextMinimalAlert(it, "Email/Username")
+            showTextMinimalAlert(it, "Email")
         }
 
-        //Password validation
+        // Password validation
         val passwordStream = RxTextView.textChanges(binding.etPassword)
             .skipInitialValue()
             .map { password ->
                 password.isEmpty()
             }
+
         passwordStream.subscribe {
             showTextMinimalAlert(it, "Password")
         }
 
-        //Button enable
+        // Button enable
         val invalidFieldsStream = Observable.combineLatest(
             usernameStream,
             passwordStream,
         ) { usernameInvalid: Boolean, passwordInvalid: Boolean ->
-             !usernameInvalid && !passwordInvalid
-        }
-        invalidFieldsStream.subscribe{ isValid ->
-            if(isValid) {
-                binding.btnLogin.isEnabled = true
-                binding.btnLogin.backgroundTintList = ContextCompat.getColorStateList(this, R.color.primary_color)
-            } else {
-                binding.btnLogin.isEnabled = false
-                binding.btnLogin.backgroundTintList= ContextCompat.getColorStateList(this, android.R.color.darker_gray)
-            }
+            !usernameInvalid && !passwordInvalid
         }
 
-        //Click
-        binding.btnLogin.setOnClickListener{
+        invalidFieldsStream.subscribe { isValid ->
+            binding.btnLogin.isEnabled = isValid
+            binding.btnLogin.backgroundTintList = ContextCompat.getColorStateList(
+                this@LoginActivity,
+                if (isValid) R.color.primary_color else android.R.color.darker_gray
+            )
+        }
+
+        // Click listener for login button
+        binding.btnLogin.setOnClickListener {
             val email = binding.etEmail.text.toString().trim()
             val password = binding.etPassword.text.toString().trim()
             loginUser(email, password)
         }
 
-        binding.btnLogin.setOnClickListener{
-            startActivity(Intent(this, LoginActivity::class.java))
-        }
-
-        binding.tvHaventAccount.setOnClickListener{
+        // Click listener for "Don't have an account?" text
+        binding.tvHaventAccount.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
+
+        // Configure system bars padding
         enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
     }
 
-    private fun showTextMinimalAlert(isNotValid: Boolean, text:String){
-        if(text == "Email/Username")
-            binding.etEmail.error = if(isNotValid) "$text cannot be empty!" else null
-        else if (text == "Password")
-            binding.etPassword.error = if(isNotValid) "$text cannot be empty!" else null
+    private fun showTextMinimalAlert(isNotValid: Boolean, text: String) {
+        if (text == "Email") {
+            binding.etEmail.error = if (isNotValid) "$text cannot be empty!" else null
+        } else if (text == "Password") {
+            binding.etPassword.error = if (isNotValid) "$text cannot be empty!" else null
+        }
     }
 
-    private fun loginUser(email: String, password: String){
+    private fun loginUser(email: String, password: String) {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { login ->
-                if(login.isSuccessful){
+                if (login.isSuccessful) {
                     Intent(this, HomeActivity::class.java).also {
                         it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                         startActivity(it)
-                        Toast.makeText(this, "Login completed successfully", Toast.LENGTH_SHORT).show()
                     }
+                    Toast.makeText(this, "Login completed successfully", Toast.LENGTH_SHORT).show()
                 } else {
                     Toast.makeText(this, login.exception?.message, Toast.LENGTH_SHORT).show()
                 }
