8ea961c96e312192092f0bac1e0abb5eeb0084c6
nuochka <140435213+nuochka@users.noreply.github.com>
2024-04-02 18:57:38 +0200

    Added ui for logout page from navigation drawer

--- app/src/main/java/com/app/lingcompanion/ui/logout/LogoutFragment.kt
+++ app/src/main/java/com/app/lingcompanion/ui/logout/LogoutFragment.kt
@@ -1,38 +1,55 @@
 package com.app.lingcompanion.ui.logout
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import com.app.lingcompanion.LoginActivity
 import com.app.lingcompanion.databinding.FragmentLogoutBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LogoutFragment : Fragment() {
-
     private var _binding: FragmentLogoutBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(LogoutViewModel::class.java)
-
         _binding = FragmentLogoutBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textLogout
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+
+        //Logout button
+        binding.btnLogout.setOnClickListener {
+            if (auth.currentUser != null) {
+                auth.signOut()
+                Intent(requireContext(), LoginActivity::class.java).also { intent ->
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                }
+                Toast.makeText(requireContext(), "Logout completed successfully", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "No user is currently signed in", Toast.LENGTH_SHORT).show()
+            }
         }
-        return root
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 
     override fun onDestroyView() {
