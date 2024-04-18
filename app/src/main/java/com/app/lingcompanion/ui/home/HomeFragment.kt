e40a2947f78e9b3337d64ca3e412edb035de8858
nuochka <140435213+nuochka@users.noreply.github.com>
2024-04-18 18:23:26 +0200

    Added a streak functional on the home page

--- app/src/main/java/com/app/lingcompanion/ui/home/HomeFragment.kt
+++ app/src/main/java/com/app/lingcompanion/ui/home/HomeFragment.kt
@@ -2,12 +2,15 @@ package com.app.lingcompanion.ui.home
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.graphics.Color
 import android.os.AsyncTask
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.app.lingcompanion.R
 import com.app.lingcompanion.databinding.FragmentHomeBinding
@@ -15,6 +18,7 @@ import org.json.JSONArray
 import org.json.JSONException
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
 
@@ -35,14 +39,86 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
         wordTextView = binding.wordTextView
-
         sharedPreferences = requireActivity().getSharedPreferences("WordFetch", Context.MODE_PRIVATE)
-
         fetchRandomWordIfNeeded()
+
+        val squares = arrayOf(
+            binding.square1, // Monday
+            binding.square2, // Tuesday
+            binding.square3, // Wednesday
+            binding.square4, // Thursday
+            binding.square5, // Friday
+            binding.square6, // Saturday
+            binding.square7  // Sunday
+        )
+
+        val currentDate = Calendar.getInstance().apply {
+            time = Date()
+        }
+        val dayOfWeek = currentDate.get(Calendar.DAY_OF_WEEK)
+        val coloredSquareIndex = (dayOfWeek + 5) % 7 // Monday is the first day in the week.
+
+        val sdf = SimpleDateFormat("yyyyMMdd", Locale.getDefault())
+        val formattedDate = sdf.format(currentDate.time)
+
+        val squaresSharedPreferences = requireActivity().getSharedPreferences("Squares", Context.MODE_PRIVATE)
+        val isSquareColoredToday = squaresSharedPreferences.getBoolean(formattedDate, false)
+
+        // Getting a current amount of the days
+        var streakCount = squaresSharedPreferences.getInt("StreakCount", 0)
+
+        if (!isSquareColoredToday) {
+            // Coloring a square
+            squares[coloredSquareIndex].setBackgroundColor(Color.rgb(142, 36, 170))
+
+            streakCount++
+
+            with(squaresSharedPreferences.edit()) {
+                putBoolean(formattedDate, true)
+                putInt("StreakCount", streakCount)
+                apply()
+            }
+
+            Toast.makeText(requireContext(), "Your today's streak was saved", Toast.LENGTH_SHORT).show()
+        } else {
+            // If a square was colored, update the color
+            squares[coloredSquareIndex].setBackgroundColor(Color.rgb(142, 36, 170))
+            streakCount++
+            streakCount = squaresSharedPreferences.getInt("StreakCount", streakCount)
+        }
+
+        val button: Button = root.findViewById(R.id.btn_streak)
+
+        button.setOnClickListener {
+            if (isSquareColoredToday) {
+                Toast.makeText(requireContext(), "You have already marked your progress", Toast.LENGTH_SHORT).show()
+            } else {
+                for (square in squares) {
+                    square.setBackgroundColor(Color.rgb(142, 36, 170))
+                }
+
+                // If user skips one day, counter is reset to zero
+                streakCount = 0
+
+                with(squaresSharedPreferences.edit()) {
+                    putBoolean(formattedDate, true)
+                    putInt("StreakCount", streakCount)
+                    apply()
+                }
+
+                Toast.makeText(requireContext(), "Your today's streak was saved", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val userStreakTextView: TextView = root.findViewById(R.id.user_streak)
+        userStreakTextView.text = "Your streak: $streakCount days"
 
         return root
     }
 
+
+
+    //Getting a random word from a list
     private fun fetchRandomWordIfNeeded() {
         val lastFetchTime = sharedPreferences.getLong("lastFetchTime", 0)
         val currentTime = System.currentTimeMillis()
@@ -72,10 +148,11 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
+
+    //The word of the day
+
     inner class DictionaryApiTask : AsyncTask<String, Void, Pair<String, String>>() {
 
-
-        //Getting a word from API
         override fun doInBackground(vararg params: String?): Pair<String, String>? {
             val apiUrl = params[0]
             apiUrl?.let {
@@ -118,6 +195,7 @@ class HomeFragment : Fragment() {
             }
         }
 
+
         private fun saveResultsToStorage(definition: String, example: String) {
             sharedPreferences.edit().apply {
                 putString("definition", definition)
@@ -148,4 +226,3 @@ class HomeFragment : Fragment() {
         }
     }
 }
-
